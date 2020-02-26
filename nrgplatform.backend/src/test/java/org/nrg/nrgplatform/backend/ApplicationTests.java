295e06598d6acba9aadad47007bc9e98629cfd51
Reza <rzgholizadeh@gmail.com>
2020-02-26 15:06:43 -0700

    chore: Create the spring-boot project with basic dependencies

---
+++ nrgplatform.backend/src/test/java/org/nrg/nrgplatform/backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nrg.nrgplatform.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

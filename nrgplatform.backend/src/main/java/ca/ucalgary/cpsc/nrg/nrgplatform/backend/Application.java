164a94ff9365445b1bdeb1bea55df76a1aa0680b
Reza <rzgholizadeh@gmail.com>
2020-02-27 17:03:26 -0700

    chore: Update package names and POM file

--- nrgplatform.backend/src/main/java/ca/ucalgary/cpsc/nrg/nrgplatform/backend/Application.java
+++ nrgplatform.backend/src/main/java/ca/ucalgary/cpsc/nrg/nrgplatform/backend/Application.java
@@ -1,4 +1,4 @@
-package org.nrg.nrgplatform.backend;
+package ca.ucalgary.cpsc.nrg.nrgplatform.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

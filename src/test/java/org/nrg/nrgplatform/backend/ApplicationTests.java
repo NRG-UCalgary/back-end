ff96ad6bd4a1b339ffe95e4512feb573b19c2e09
Reza <rzgholizadeh@gmail.com>
2020-03-04 22:29:41 -0700

    test: Update test class for controller

--- src/test/java/org/nrg/nrgplatform/backend/ApplicationTests.java
+++ src/test/java/org/nrg/nrgplatform/backend/ApplicationTests.java
@@ -2,7 +2,9 @@ package org.nrg.nrgplatform.backend;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import ca.ucalgary.cpsc.nrg.nrgplatform.backend.Application;
+
+@SpringBootTest(classes = Application.class)
 class ApplicationTests {
 
 }
